2e2a0d286dd9c38a2a74997818c0d1624a6508cf
Theofedericiolivieri <112435873+Theofedericiolivieri@users.noreply.github.com>
2023-05-26 18:57:02 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+objects=[];
+status="";
+function setup(){
+    canvas= createCanvas(480, 380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide()
+}
+function start(){
+    objectDetector=ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML="Status: Detectando Objetos";
+}
